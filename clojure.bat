eb6df7b98a0c12e61bc928a52e5a83ded9b32084
Michael Chen <cwchen123@gmail.com>
2020-04-23 19:18:00 +0800

    Create clojure.bat

---
+++ clojure.bat
@@ -0,0 +1,38 @@
+@echo off
+rem Wrapper for Clojure on Windows
+rem Copyright (c) 2020 Michael Chen
+rem Licensed under MIT
+
+rem Place clojure.bat, build.bat and clean.bat at the root path of Clojure repo.
+
+rem Check whether Java is available.
+java -version 1>nul 2>&1 || (
+    echo No Java on the system >&2
+    exit /B 1
+)
+
+set cwd=%CD%
+set rootdir=%~dp0
+
+rem Set extra classpath as needed.
+if "x%CLASSPATH%" == "x" (
+    set extra_classpath=";."
+) else (
+    set extra_classpath=";%CLASSPATH%;."
+)
+
+rem Check whether compiled Clojure is available.
+rem If Clojure is not available, compile it from scratch
+if not exist %rootdir%clojure.jar (
+    echo No compiled Clojure on the system >&2
+    echo Compile Clojure from scratch >&2
+
+    call %rootdir%build.bat
+    cd %cwd%
+
+    echo Restart Clojure >&2
+    exit /B 0
+)
+
+rem Run Clojure
+java -cp %rootdir%clojure.jar%extra_classpath% clojure.main %*
