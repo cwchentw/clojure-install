118308aa3a2224a1dec651535aa3d9f3815ac407
Michael Chen <cwchen123@gmail.com>
2020-04-23 19:19:24 +0800

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -7,5 +7,6 @@ rem Place clojure.bat, build.bat and clean.bat at the root path of Clojure repo.
 
 set rootdir=%~dp0
 
-del /S %rootdir%clojure.jar %rootdir%apache-maven-3.6.3-bin.zip
-rmdir /S /Q %rootdir%apache-maven-3.6.3
+if exist %rootdir%clojure.jar del /S %rootdir%clojure.jar
+if exist %rootdir%apache-maven-3.6.3-bin.zip del /S %rootdir%apache-maven-3.6.3-bin.zip
+if exist %rootdir%apache-maven-3.6.3 rmdir /S /Q %rootdir%apache-maven-3.6.3
