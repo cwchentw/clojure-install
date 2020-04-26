18bf9ec04f613fe163a9b06d0fd8dc75ae7d7625
Michael Chen <cwchen.tw@outlook.com>
2020-04-27 05:38:11 +0800

    Minor update

--- clojure/clojure.bat
+++ clojure/clojure.bat
@@ -4,7 +4,7 @@ rem Copyright (c) 2020 Michael Chen.
 rem Licensed under MIT.
 
 
-powershell -Command "Get-Command -Name Invoke-Clojure -ErrorAction SilentlyContinue"
+powershell -Command "Get-Command -Name Invoke-Clojure -ErrorAction SilentlyContinue" 1>nul 2>&1
 if "%ERRORLEVEL%" == "0" (
     powershell -Command Invoke-Clojure %*
     exit /B %ERRORLEVEL%
