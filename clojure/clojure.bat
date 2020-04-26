e457fe63d480d33b058ba23496d9fe500de75866
Michael Chen <cwchen.tw@outlook.com>
2020-04-26 20:40:33 +0800

    Add aliases to official Clojure cli tool on Windows

---
+++ clojure/clojure.bat
@@ -0,0 +1,14 @@
+@echo off
+rem Alias to official Clojure cli tool.
+rem Copyright (c) 2020 Michael Chen.
+rem Licensed under MIT.
+
+
+powershell -Command "Get-Command -Name Invoke-Clojure -ErrorAction SilentlyContinue"
+if "%ERRORLEVEL%" == "0" (
+    powershell -Command Invoke-Clojure %*
+    exit /B %ERRORLEVEL%
+) else (
+    echo No official Clojure cli tool on the system >&2
+    exit /B 1
+)
