3febb1a50797fdd984e9ee1cca9a8afffe7c291e
Michael Chen <cwchen.tw@outlook.com>
2020-04-27 08:08:33 +0800

    Refactor some code

--- jline1/build.bat
+++ jline1/build.bat
@@ -18,10 +18,19 @@ powershell -Help 1>nul 2>&1 || (
 rem Download Maven.
 powershell -Command "Invoke-WebRequest -Uri https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip -OutFile %rootdir%apache-maven-3.6.3-bin.zip"
 
+if not exist %rootdir%apache-maven-3.6.3-bin.zip (
+    echo Failed to download %rootdir%apache-maven-3.6.3-bin.zip >&2
+    exit /B 1
+)
+
 rem Unzip Maven.
 powershell -Command "Expand-Archive -Path %rootdir%apache-maven-3.6.3-bin.zip -DestinationPath %rootdir% "
+
+if not exist %rootdir%apache-maven-3.6.3 (
+    echo Failed to extract maven to %rootdir%apache-maven-3.6.3 >&2
+    exit /B 1
+)
 
 set mvn=%rootdir%apache-maven-3.6.3\bin\mvn.cmd
 
 %mvn% -Dmaven.test.skip=true package
-
